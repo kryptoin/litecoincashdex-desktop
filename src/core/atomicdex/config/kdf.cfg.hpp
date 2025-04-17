8b4f5afa139d52efaee10bb0f24e918bd420ff01
ShorelineCrypto <admin@shorelinecrypto.com>
2025-04-16 20:49:13 -0400

    add mm2 seednode IP

--- src/core/atomicdex/config/kdf.cfg.hpp
+++ src/core/atomicdex/config/kdf.cfg.hpp
@@ -34,7 +34,7 @@ namespace atomic_dex
         std::string              gui{std::string(DEX_NAME) + " "s + atomic_dex::get_version()};
         int64_t                  netid{8762};
         int64_t                  rpcport{atomic_dex::g_dex_rpcport};
-        std::vector<std::string> seednodes{};
+        std::vector<std::string> seednodes{"46.4.78.11", "46.4.87.18", "188.241.240.110", "66.42.60.49", "209.145.50.223"};
         //std::vector<std::string> seednodes{"195.201.91.96", "195.201.91.53", "168.119.174.126", "46.4.78.11", "46.4.87.18"};
         //std::vector<std::string> seednodes{"46.4.78.11", "46.4.87.18"};
 #ifdef _WIN32
