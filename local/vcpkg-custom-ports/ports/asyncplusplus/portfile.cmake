9d1806bd9a3e41f0510dbe4626647733e6624bb7
interblock <fgabrielpena@gmail.com>
2026-06-17 14:01:51 +0000

    Patch asyncplusplus for C++20

--- local/vcpkg-custom-ports/ports/asyncplusplus/portfile.cmake
+++ local/vcpkg-custom-ports/ports/asyncplusplus/portfile.cmake
@@ -8,6 +8,15 @@ vcpkg_from_github(
     SHA512 f0d5d746ff8b0e76a6b143420347883f000f4741d536096548df829a33641cbdbea0fb37bf238314350653e6207cf53ee32826863b19d940bae5380415a22db9
 )
 
+
+# C++20 removes std::result_of; asyncplusplus 1.2 still uses it.
+file(GLOB_RECURSE ASYNCPLUSPLUS_HEADERS "${SOURCE_PATH}/include/*.h")
+foreach(header ${ASYNCPLUSPLUS_HEADERS})
+    file(READ "${header}" header_content)
+    string(REPLACE "std::result_of<" "std::invoke_result<" header_content "${header_content}")
+    file(WRITE "${header}" "${header_content}")
+endforeach()
+
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
