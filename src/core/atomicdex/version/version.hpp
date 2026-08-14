d851586c1428aebe42137cb978bae43d469fe6a7
Kryptoin <kryptoin@users.noreply.github.com>
2026-08-14 13:04:10 -0700

    bump version

--- src/core/atomicdex/version/version.hpp
+++ src/core/atomicdex/version/version.hpp
@@ -21,7 +21,7 @@ namespace atomic_dex
     constexpr const char*
     get_version()
     {
-        return "0.9.6-beta";
+        return "0.9.7-beta";
     }
 
     constexpr int
@@ -33,7 +33,7 @@ namespace atomic_dex
     constexpr const char*
     get_raw_version()
     {
-        return "0.9.6";
+        return "0.9.7";
     }
 
     constexpr const char*
